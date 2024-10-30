2368ca4024005e6a782e62aa71d71fe8d189bf72
KeyeSko <aygiztaipoav@gmail.com>
2024-10-30 18:00:40 +0300

    1lab fix_2

--- lab1/main.c
+++ lab1/main.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#define ZIP_END_SIG 0x06054B50
+#define CD_FILE_HEADER_SIG 0x02014B50
 
 #pragma pack(push, 1)
-struct LocalFileHeader
-{
-    // Обязательная сигнатура, равна 0x04034b50
+typedef struct {
+    // Обязательная сигнатура, равна 0x06054b50
     uint32_t signature;
+    // Номер диска
+    uint16_t diskNumber;
+    // Номер диска, где находится начало Central Directory
+    uint16_t startDiskNumber;
+    // Количество записей в Central Directory в текущем диске
+    uint16_t numberCentralDirectoryRecord;
+    // Всего записей в Central Directory
+    uint16_t totalCentralDirectoryRecord;
+    // Размер Central Directory
+    uint32_t sizeOfCentralDirectory;
+    // Смещение Central Directory
+    uint32_t centralDirectoryOffset;
+    // Длина комментария
+    uint16_t commentLength;
+    // Комментарий (длиной commentLength)
+    //uint8_t *comment;
+} EOCD;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct {
+    // Обязательная сигнатура, равна 0x02014b50 
+    uint32_t signature;
+    // Версия для создания
+    uint16_t versionMadeBy;
     // Минимальная версия для распаковки
     uint16_t versionToExtract;
     // Битовый флаг
@@ -27,55 +55,110 @@ struct LocalFileHeader
     uint16_t filenameLength;
     // Длина поля с дополнительными данными
     uint16_t extraFieldLength;
-    // Название файла (размером filenameLength)
+    // Длина комментариев к файлу
+    uint16_t fileCommentLength;
+    // Номер диска
+    uint16_t diskNumber;
+    // Внутренние аттрибуты файла
+    uint16_t internalFileAttributes;
+    // Внешние аттрибуты файла
+    uint32_t externalFileAttributes;
+    // Смещение до структуры LocalFileHeader
+    uint32_t localFileHeaderOffset;
+    // Имя файла (длиной filenameLength)
     //uint8_t *filename;
-    // Дополнительные данные (размером extraFieldLength)
+    // Дополнительные данные (длиной extraFieldLength)
     //uint8_t *extraField;
-};
+    // Комментарий к файла (длиной fileCommentLength)
+    //uint8_t *fileComment;
+} CentralDirectoryFileHeader;
 #pragma pack(pop)
 
-int main(int argc, char *argv[]){
-    
-    int signature[4] = { 0x50, 0x4b, 0x03, 0x04};
-    int ch = 0;
-    char fileName[255] = {0};
-    int flag = 0;
-    int i = 0;
+int find_zip_end(FILE *file, long *end_offset) {
+    fseek(file, -22, SEEK_END);
+    *end_offset = ftell(file);
 
-    if(argc == 1){
-    	printf("Input filename\n");
-    }
-
-    FILE *fp = fopen(argv[1], "rb");
-    if(fp == NULL){
-        perror("main");
-        return 0;
-    }
-    
-    struct LocalFileHeader readFile;
-
-    while ((ch = fgetc(fp)) != EOF){
-        if (ch == signature[i]){
-            i++;
-            if (i == 4){
-                flag = 1;
-                i = 0;
-                fseek(fp, -4, SEEK_CUR);
-                if (1 != fread(&readFile, sizeof(readFile), 1, fp))
-                    return 0;
-                if (1 != fread(fileName, readFile.filenameLength, 1, fp))
-                    return 0;
-                fileName[readFile.filenameLength] = 0;
-                printf("fileName: %s\n", fileName);
-            }
-        } else {
-            i = (ch == signature[0])? 1 : 0;
+    uint32_t signature;
+    while (*end_offset > 0) {
+        fread(&signature, sizeof(signature), 1, file);
+        if (signature == ZIP_END_SIG) {
+            fseek(file, -4, SEEK_CUR);
+            return 1;
         }
+        fseek(file, -5, SEEK_CUR);
+        (*end_offset)--;
     }
-
-    if(!flag)
-        printf("File doesn't have zip\n");
-
-    fclose(fp);
     return 0;
 }
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "rb");
+    if (!file) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    long zip_offset;
+    if (find_zip_end(file, &zip_offset)) {
+        printf("Zip archive found!\nFiles in archive:\n");
+        fseek(file, zip_offset, SEEK_SET);
+        EOCD zip_end;
+        if (fread(&zip_end, sizeof(zip_end), 1, file) != 1)
+            return 1;
+
+        if (zip_end.signature != ZIP_END_SIG) {
+            perror("Invalid EOCD signature");
+            fclose(file);
+            return 1;
+        }
+        if (zip_end.diskNumber != 0 || zip_end.startDiskNumber != 0 || zip_end.numberCentralDirectoryRecord != zip_end.totalCentralDirectoryRecord) {
+            perror("Invalid EOCD values");
+            fclose(file);
+            return 1;
+        }
+
+        long cd_absolute_offset = zip_offset - zip_end.sizeOfCentralDirectory;
+        if (cd_absolute_offset < 0) {
+            fprintf(stderr, "Invalid Central Directory offset.\n");
+            fclose(file);
+            return 1;
+        }
+
+        fseek(file, cd_absolute_offset, SEEK_SET);
+        for (int i = 0; i < zip_end.totalCentralDirectoryRecord; i++) {
+            char filename[256];
+            CentralDirectoryFileHeader cd_header;
+
+            if (fread(&cd_header, sizeof(cd_header), 1, file) != 1)
+                return 1;
+            if (cd_header.signature != CD_FILE_HEADER_SIG) {
+                perror("Invalid Central Directory File Header signature");
+                fclose(file);
+                return 1;
+            }
+            if (cd_header.filenameLength > 255) {
+                perror("Filename too long");
+                fclose(file);
+                return 1;
+            }
+
+            fread(filename, 1, cd_header.filenameLength, file);
+            filename[cd_header.filenameLength] = '\0';
+            printf("%s\n", filename);
+
+            fseek(file, cd_header.extraFieldLength + cd_header.fileCommentLength, SEEK_CUR);
+        }
+    } else {
+        printf("No zip archive found.\n");
+    }
+
+    fclose(file);
+    return 0;
+}
+
